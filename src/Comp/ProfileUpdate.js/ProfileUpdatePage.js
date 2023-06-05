cb8e8eb36afc3e8ec5b00f1026d1faef8bd9f5f4
VS1405 <varshamhaske808@gmail.com>
2023-06-05 09:51:25 +0530

    axios get

--- src/Comp/ProfileUpdate.js/ProfileUpdatePage.js
+++ src/Comp/ProfileUpdate.js/ProfileUpdatePage.js
@@ -1,4 +1,4 @@
-import React , {useRef,  useState, Fragment, useContext} from 'react';
+import React , {useRef,  useState, Fragment, useContext, useEffect} from 'react';
 import classes from './ProfileUpdatePage.module.css';
 import axios from 'axios'
 
@@ -7,12 +7,12 @@ import AuthContect from '../Login/store/auth-context';
 
 const ProfileUpdatePage = () => {
 
-const AuthCtx = useContext(AuthContect)
- const [name, setName] =   useState('')
- const [profileUrl, setProfileUrl] =   useState('')
+const AuthCtx = useContext(AuthContect);
 
-   const nameRef = useRef('');
-   const profileUrfRef = useRef('');
+const [dataList, setDatalist] = useState([])
+
+   const nameRef = useRef();
+   const profileUrfRef = useRef();
 
    const submitHandler = (e) =>{
     e.preventDefault();
@@ -32,11 +32,33 @@ const AuthCtx = useContext(AuthContect)
     )
       .then(response => {
         console.log(response.data);
+        fetchDataList();
       })
       .catch(error => {
-        console.log(error + ' something went wrong');
+        console.log(error);
+        console.log('something went wrong')
       });
     
+   }
+
+   useEffect(()=>{
+    fetchDataList()
+   }, []);
+
+   const fetchDataList = ()=>{
+       // Fetch the list of data from the server
+    axios.get('https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyABurclSGsl2V7_fg6o1LjbqWTHge3w4yA')
+    .then((resp)=>{
+      console.log(resp.data);
+      setDatalist(resp.data);   // Update the dataList state with the retrieved data
+      console.log('fetch responce data');
+    })
+    .catch((error) => {
+      console.log(error.message)
+      console.log(error.name);
+      console.log(error.code);
+      console.log('error in fetching data');
+    })
    }
 
   return (
@@ -46,22 +68,31 @@ const AuthCtx = useContext(AuthContect)
       <div className={classes.container}>
         <div className={classes.header}>
             <header>Contact Details</header>
-            <button >Cancel</button>
+            <button className={classes.btn}>Cancel</button>
         </div>
         <div className={classes.inputDetails}>
           <div className={classes.row}>
             <label>Full Name</label>
-            <input type='text' ref={nameRef}  onChange={e => setName(e.target.value)}/>
+            <input type='text' ref={nameRef}  />
 
           </div>
           <div className={classes.row}>
             <label>Profile photo Url</label>
-            <input type='text' name='profileUrl' ref={profileUrfRef} onChange={e => setProfileUrl( e.target.value)}/>
+            <input type='text' name='profileUrl' ref={profileUrfRef} />
           </div>
         </div>
       </div>
       <button type='submit' className={classes.button}>Update</button>
     </form>
+
+<div className={classes.list}>
+  <h2>List</h2>
+    <ul>
+      {dataList.map((item)=>{
+        <li key={item.idToken}>{item.name}</li>
+      })}
+    </ul>
+</div>
     </Fragment>
   )
 }
